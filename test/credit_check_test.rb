fe3cba66be71a64898a1be6fdc8e06ea0ba9dd9d
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-05-06 14:49:53 -0600

    added a checker to see if the card is valid and invalid, also tested to see if I can test the validity of discover cards

--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -51,6 +51,28 @@ class CreditCheckTest < Minitest::Test
 
     numbers_subtracted_by_nine_array = [3, 8, 5, 0, 5, 4, 7, 5, 4, 1, 3, 5, 9, 4, 9, 8]
 
-    assert_equal 0, new_card.checking_modulo(numbers_subtracted_by_nine_array)
+    assert_equal 0, new_card.will_the_modulo_return_zero(numbers_subtracted_by_nine_array)
+  end
+
+  def test_that_it_will_print_valid_or_not_if_invalid
+      new_card = CreditCheck.new("4929735477250543")
+
+      numbers_subtracted_by_nine_array = [3, 8, 5, 0, 5, 4, 7, 5, 4, 1, 3, 5, 9, 4, 9, 8]
+
+      assert "Valid", new_card.checking_validity(numbers_subtracted_by_nine_array)
+  end
+
+  def test_if_it_takes_discover_card
+  new_card = CreditCheck.new("342804633855673")
+
+  numbers_subtracted_by_nine_array = [3, 14, 6, 10, 5, 16, 3, 6, 6, 8, 0, 16, 2, 8, 3]
+  assert "Valid", new_card.checking_validity(numbers_subtracted_by_nine_array )
+  end
+
+  def test_that_this_discover_card_is_invalid
+
+    new_card = CreditCheck.new("342801633855673")
+    numbers_subtracted_by_nine_array = [3, 14, 6, 10, 5, 16, 3, 6, 6, 8, 0, 16, 2, 8, 3]
+    assert "Invalid Card Mother Fucker",  new_card.checking_validity(numbers_subtracted_by_nine_array )
   end
 end
