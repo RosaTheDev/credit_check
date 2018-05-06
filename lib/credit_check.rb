df02630bbbbbff5574edafd6deeb1d8aeb4de06d
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-05-06 07:14:40 -0600

    the double_every_odd_index method takes an arugment which is the return value of the initialize method, then I took the value of the arugment and called the .map enumerable along with .with_index because we only want to double the odd index's, and I also put a conditional in the block because if we do not double the even index in an array then we want to leave it alone

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -3,4 +3,14 @@ class CreditCheck
   def initialize(card_number)
     @card_number = card_number.to_i.digits
   end
+
+  def double_every_odd_index(card_number_array)
+    card_number_array.map.with_index do |card_num, index|
+      if index.odd?
+        card_num * 2
+      else
+        card_num
+      end
+    end
+  end
 end
