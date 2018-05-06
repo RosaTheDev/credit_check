02742eef4900d61b1ba35dd008f842c03f8d7009
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-05-06 07:06:55 -0600

    new test that checks to see if every other index in the array is doubled

--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -11,9 +11,18 @@ class CreditCheckTest < Minitest::Test
 
   def test_that_the_card_number_is_converted_into_an_array
     new_card = CreditCheck.new("4929735477250543")
-    
+
     expected = [3, 4, 5, 0, 5, 2, 7, 7, 4, 5, 3, 7, 9, 2, 9, 4]
 
     assert_equal expected, new_card.card_number
+  end
+
+  def test_that_every_odd_index_in_the_array_is_doubled
+    new_card = CreditCheck.new("4929735477250543")
+
+    new_card_array = [3, 4, 5, 0, 5, 2, 7, 7, 4, 5, 3, 7, 9, 2, 9, 4]
+
+    expected = [3, 8, 5, 0, 5, 4, 7, 14, 4, 10, 3, 14, 9, 4, 9, 8]
+    assert_equal expected, new_card.double_every_odd_index(new_card_array)
   end
 end
