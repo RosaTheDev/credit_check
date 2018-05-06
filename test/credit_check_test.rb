4ddfe4c5fbfb212ff1988834f70480d85f1a1145
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-05-06 07:00:14 -0600

    added a second test, I want to see if my card_number instance variable converts the argument card_number into an array

--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -8,4 +8,12 @@ class CreditCheckTest < Minitest::Test
 
     assert_instance_of CreditCheck, new_card
   end
+
+  def test_that_the_card_number_is_converted_into_an_array
+    new_card = CreditCheck.new("4929735477250543")
+    
+    expected = [3, 4, 5, 0, 5, 2, 7, 7, 4, 5, 3, 7, 9, 2, 9, 4]
+
+    assert_equal expected, new_card.card_number
+  end
 end
